fce9e37e19b082489e03ea18edbce92d795804fe
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-10-12 14:47:07 +0700

    Add comment

--- app/src/main/java/com/hoc/comicapp/domain/repository/DownloadComicsRepository.kt
+++ app/src/main/java/com/hoc/comicapp/domain/repository/DownloadComicsRepository.kt
@@ -9,15 +9,40 @@ import io.reactivex.Observable
 import kotlinx.coroutines.flow.Flow
 
 interface DownloadComicsRepository {
+  /**
+   * Download chapter by [chapterLink]
+   * @param chapterLink chapter url
+   * @return Flow of progress (from 0 to 100)
+   */
   fun downloadChapter(chapterLink: String): Flow<Int>
 
+  /**
+   * Get all downloaded comics
+   * @return observable
+   */
   fun getDownloadedComics(): Observable<Either<ComicAppError, List<DownloadedComic>>>
 
+  /**
+   * Get downloaded comic by [link]
+   * @return observable
+   */
   fun getDownloadedComic(link: String): Observable<Either<ComicAppError, DownloadedComic>>
 
+  /**
+   * Get all downloaded chapters
+   * @return live data
+   */
   fun getDownloadedChapters() : LiveData<List<DownloadedChapter>>
 
+  /**
+   * Delete downloaded [chapter]
+   * @return either
+   */
   suspend fun deleteDownloadedChapter(chapter: DownloadedChapter): Either<ComicAppError, Unit>
 
+  /**
+   * Delete downloaded [comic]
+   * @return either
+   */
   suspend fun deleteComic(comic: DownloadedComic): Either<ComicAppError, Unit>
 }
