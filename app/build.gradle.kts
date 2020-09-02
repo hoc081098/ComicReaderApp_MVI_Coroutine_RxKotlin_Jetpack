b7c226c1dcd6f63c29938a79ef233f3f91a09bd2
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-09-02 23:41:36 +0700

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,22 +1,6 @@
 import org.jetbrains.kotlin.gradle.internal.AndroidExtensionsFeature.PARCELIZE
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion: String by rootProject.extra
-val navVersion: String by rootProject.extra
-val coroutinesVersion: String by rootProject.extra
-val lifecycleVersion: String by rootProject.extra
-val pagingVersion: String by rootProject.extra
-val koinVersion: String by rootProject.extra
-val materialVersion: String by rootProject.extra
-val glideVersion: String by rootProject.extra
-val rxBindingVersion: String by rootProject.extra
-val timberVersion: String by rootProject.extra
-val rxRelayVersion: String by rootProject.extra
-val threetenabpVersion: String by rootProject.extra
-val retrofit2Version: String by rootProject.extra
-val roomVersion: String by rootProject.extra
-val workVersion: String by rootProject.extra
-
 plugins {
   id("com.android.application")
   id("kotlin-android")
@@ -30,21 +14,28 @@ androidExtensions {
 }
 
 android {
-  compileSdkVersion(30)
+  buildToolsVersion(versions.sdk.buildTools)
+  compileSdkVersion(versions.sdk.compile)
 
   defaultConfig {
-    applicationId = "com.hoc.comicapp"
-    minSdkVersion(21)
-    targetSdkVersion(30)
-    versionCode = 1
-    versionName = "1.0"
+    applicationId = appConfig.applicationId
+
+    minSdkVersion(versions.sdk.min)
+    targetSdkVersion(versions.sdk.target)
+
+    versionCode = appConfig.versionCode
+    versionName = appConfig.versionName
+
+    resConfigs(appConfig.supportedLocales)
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
   buildTypes {
     getByName("release") {
-      isMinifyEnabled = false
+      isMinifyEnabled = true
+      isShrinkResources = true
+
       proguardFiles(
         getDefaultProguardFile("proguard-android-optimize.txt"),
         "proguard-rules.pro"
@@ -101,68 +92,58 @@ dependencies {
   implementation(deps.androidX.room.ktx)
   implementation(deps.androidX.room.rxJava3)
 
-  // Work manager
-  implementation("androidx.work:work-runtime-ktx:$workVersion") // Kotlin + coroutines
+  implementation(deps.androidX.work.runtimeKtx)
 
-  // Koin
-  implementation("org.koin:koin-androidx-viewmodel:$koinVersion")
-  implementation("org.koin:koin-androidx-scope:$koinVersion")
+  implementation(deps.koin.androidXViewModel)
+  implementation(deps.koin.androidXScope)
 
-  // Moshi, Retrofit, OkHttp
-  implementation("com.squareup.moshi:moshi-kotlin:1.9.2")
-  implementation("com.squareup.retrofit2:retrofit:$retrofit2Version")
-  implementation("com.squareup.retrofit2:converter-moshi:$retrofit2Version")
-  implementation("com.squareup.okhttp3:logging-interceptor:4.8.0")
+  implementation(deps.moshiKotlin)
+  implementation(deps.okHttpLoggingInterceptor)
+  implementation(deps.retrofit.retrofit)
+  implementation(deps.retrofit.converterMoshi)
 
-  // Leak canary
-  debugImplementation("com.squareup.leakcanary:leakcanary-android:2.4")
+  debugImplementation(deps.leakCanaryAndroid)
+  implementation(deps.timber)
 
-  // RxRelay, RxBinding, Timber
-  implementation("com.jakewharton.rxrelay3:rxrelay:$rxRelayVersion")
-  implementation("com.jakewharton.rxbinding4:rxbinding:$rxBindingVersion")
-  implementation("com.jakewharton.rxbinding4:rxbinding-core:$rxBindingVersion")
-  implementation("com.jakewharton.rxbinding4:rxbinding-material:$rxBindingVersion")
-  implementation("com.jakewharton.rxbinding4:rxbinding-swiperefreshlayout:$rxBindingVersion")
-  implementation("com.jakewharton.rxbinding4:rxbinding-recyclerview:$rxBindingVersion")
-  implementation("com.jakewharton.timber:timber:$timberVersion")
+  implementation(deps.rxBinding.platform)
+  implementation(deps.rxBinding.core)
+  implementation(deps.rxBinding.material)
+  implementation(deps.rxBinding.swipeRefreshLayout)
+  implementation(deps.rxBinding.recyclerView)
 
-  // Rx
-  implementation("io.reactivex.rxjava3:rxkotlin:3.0.0")
-  implementation("io.reactivex.rxjava3:rxjava:3.0.6")
-  implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
+  implementation(deps.reactiveX.kotlin)
+  implementation(deps.reactiveX.java)
+  implementation(deps.reactiveX.android)
+  implementation(deps.rxRelay)
 
-  // Glide
-  implementation("com.github.bumptech.glide:glide:$glideVersion")
-  kapt("com.github.bumptech.glide:compiler:$glideVersion")
-  implementation("com.github.bumptech.glide:okhttp3-integration:$glideVersion") {
-    exclude(group = "glide-parent")
-  }
+  implementation(deps.glide.glide)
+  kapt(deps.glide.compiler)
+  implementation(deps.glide.integration) { exclude(group = "glide-parent") }
 
-  // Views
-  implementation("com.miguelcatalan:materialsearchview:1.4.0")
-  implementation("com.ms-square:expandableTextView:0.1.4")
-  implementation("com.jaredrummler:material-spinner:1.3.1")
-  implementation("com.github.antonKozyriatskyi:CircularProgressIndicator:1.3.0")
-  implementation("com.github.chrisbanes:PhotoView:2.3.0")
-  implementation("com.chauthai.swipereveallayout:swipe-reveal-layout:1.4.1")
-  implementation("de.hdodenhof:circleimageview:3.1.0")
-  implementation("com.amulyakhare:com.amulyakhare.textdrawable:1.0.1")
+  implementation(deps.customView.materialSearchView)
+  implementation(deps.customView.expandableTextView)
+  implementation(deps.customView.materialSpinner)
+  implementation(deps.customView.circularProgressIndicator)
+  implementation(deps.customView.photoView)
+  implementation(deps.customView.swipeRevealLayout)
+  implementation(deps.customView.circleImageView)
+  implementation(deps.customView.textDrawable)
 
   testImplementation("junit:junit:4.13")
-  androidTestImplementation("androidx.test:runner:1.3.0-alpha05")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0-alpha05")
+  androidTestImplementation("androidx.test:runner:1.3.0")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 
-  // Firebase
-  implementation("com.google.firebase:firebase-auth:19.3.2")
-  implementation("com.google.firebase:firebase-storage:19.2.0")
-  implementation("com.google.firebase:firebase-firestore:21.6.0")
-  implementation("com.google.firebase:firebase-analytics:17.5.0")
-  implementation("com.google.firebase:firebase-crashlytics:17.2.1")
+  implementation(deps.firebase.auth)
+  implementation(deps.firebase.storage)
+  implementation(deps.firebase.firestore)
+  implementation(deps.firebase.analytics)
+  implementation(deps.firebase.crashlytics.crashlytics)
 
-  implementation("com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava")
-
-  implementation("com.github.hoc081098:ViewBindingDelegate:0.0.2")
+  implementation(deps.listenableFuture)
+  implementation(deps.viewBindingDelegate)
 }
 
-apply(plugin = "com.google.gms.google-services")
-apply(plugin = "com.google.firebase.crashlytics")
+apply {
+  plugin("com.google.gms.google-services")
+  plugin("com.google.firebase.crashlytics")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-  val kotlinVersion by extra("1.4.0")
-  val navVersion by extra("2.3.0")
-  extra.run {
-    set("coroutinesVersion", "1.3.9")
-    set("lifecycleVersion", "2.3.0-alpha07")
-    set("koinVersion", "2.2.0-alpha-1")
-    set("materialVersion", "1.2.0")
-    set("glideVersion", "4.11.0")
-    set("rxBindingVersion", "4.0.0")
-    set("timberVersion", "4.7.1")
-    set("rxRelayVersion", "3.0.0")
-    set("retrofit2Version", "2.9.0")
-    set("roomVersion", "2.3.0-alpha02")
-    set("workVersion", "2.5.0-alpha01")
-  }
-
   repositories {
     google()
     jcenter()
     maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:4.0.1")
-    classpath(kotlin("gradle-plugin", kotlinVersion))
-    classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
-    classpath("com.google.gms:google-services:4.3.3")
-    classpath("com.google.firebase:firebase-crashlytics-gradle:2.2.1")
+    classpath(deps.sdk.classpath)
+    classpath(deps.kotlin.classpath)
+    classpath(deps.androidX.navigation.classpath)
+    classpath(deps.firebase.classpath)
+    classpath(deps.firebase.crashlytics.classpath)
   }
 }
 
